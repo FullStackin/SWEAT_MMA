5777f137e12120452066937e65e0876296620685
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-18 09:39:07 -0700

    docs: Add .env file

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,20 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "2271182148msh649728d0796455ep17e823jsn0dd054fb73c4",
+    "X-RapidAPI-Key": process.env.REACT_APP_EXERCISE_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
+
 export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": "e6503bb7b7msh06cf7f77f3f4d6ep1573b8jsn6014e0fab73a",
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
   },
 };
 
-export const fetchData = async (url, options) => {
+export async function fetchData(url, options) {
   try {
     const response = await fetch(url, options);
     const data = await response.json();
@@ -27,4 +28,4 @@ export const fetchData = async (url, options) => {
     console.error("Error fetching data:", error);
     return { response: null, data: [] };
   }
-};
+}
